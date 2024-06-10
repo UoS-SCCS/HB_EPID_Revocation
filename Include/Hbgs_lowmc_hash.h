50be9379699b4ac6f0b294f9ce0eebf9ed977e2b
Newton, Christopher Dr (Computer Science) <c.newton@surrey.ac.uk>
2024-06-10 16:36:17 +0100

    Code ready for publication

--- Include/Hbgs_lowmc_hash.h
+++ Include/Hbgs_lowmc_hash.h
@@ -1,4 +1,4 @@
-/***************************************************************************
+/*******************************************************************************
  * File:        Hbgs_hash.h
  * Description: sha3 functions used when generating the G-tree (otherwise
  *              the picnic versions are used). Generates a hash of
@@ -7,9 +7,36 @@
  * Author:      Chris Newton
  * Created:     Saturday 7 May 2022
  *
- * (C) Copyright 2022, University of Surrey.
  *
- ****************************************************************************/
+*******************************************************************************/
+
+/*******************************************************************************
+*                                                                              *
+* (C) Copyright 2020-2021 University of Surrey                                 *
+*                                                                              *
+* Redistribution and use in source and binary forms, with or without           *
+* modification, are permitted provided that the following conditions are met:  *
+*                                                                              *
+* 1. Redistributions of source code must retain the above copyright notice,    *
+* this list of conditions and the following disclaimer.                        *
+*                                                                              *
+* 2. Redistributions in binary form must reproduce the above copyright notice, *
+* this list of conditions and the following disclaimer in the documentation    *
+* and/or other materials provided with the distribution.                       *
+*                                                                              *
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  *
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    *
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   *
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE    *
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR          *
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF         *
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS     *
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN      *
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)      *
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   *
+* POSSIBILITY OF SUCH DAMAGE.                                                  *
+*                                                                              *
+*******************************************************************************/
 
 #ifndef HBGS_LOWMC_HASH_H
 #define HBGS_LOWMC_HASH_H
